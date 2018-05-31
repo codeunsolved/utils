30b5659eb9f756ba4bae0fbd687a15e123c23831
codeunsolved <codeunsolved@gmail.com>
2018-05-31 16:26:44 +0800

    add code

---
+++ code.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# PROGRAM : code
+# AUTHOR  : codeunsolved@gmail.com
+# CREATED : May 22 2018
+# VERSION : v0.0.1a
+
+
+GREEK = {
+    # Greek lower case letters
+    'alpha': '\u03B1',    # α   GREEK SMALL LETTER ALPHA
+    'beta': '\u03B2',     # β   GREEK SMALL LETTER BETA
+    'gamma': '\u03B3',    # γ   GREEK SMALL LETTER GAMMA
+    'delta': '\u03B4',    # δ   GREEK SMALL LETTER DELTA
+    'epsilon': '\u03B5',  # ε   GREEK SMALL LETTER EPSILON
+    'zeta': '\u03B6',     # ζ   GREEK SMALL LETTER ZETA
+    'eta': '\u03B7',      # η   GREEK SMALL LETTER ETA
+    'theta': '\u03B8',    # θ   GREEK SMALL LETTER THETA
+    'iota': '\u03B9',     # ι   GREEK SMALL LETTER IOTA
+    'kappa': '\u03BA',    # κ   GREEK SMALL LETTER KAPPA
+    'lamda': '\u03BB',    # λ   GREEK SMALL LETTER LAMDA
+    'mu': '\u03BC',       # μ   GREEK SMALL LETTER MU
+    'nu': '\u03BD',       # ν   GREEK SMALL LETTER NU
+    'xi': '\u03BE',       # ξ   GREEK SMALL LETTER XI
+    'omicron': '\u03BF',  # ο   GREEK SMALL LETTER OMICRON
+    'pi': '\u03C0',       # π   GREEK SMALL LETTER PI
+    'rho': '\u03C1',      # ρ   GREEK SMALL LETTER RHO
+    'final_sigma': '\u03C2',  # ς   GREEK SMALL LETTER FINAL SIGMA
+    'sigma': '\u03C3',    # σ   GREEK SMALL LETTER SIGMA
+    'tau': '\u03C4',      # τ   GREEK SMALL LETTER TAU
+    'upsilon': '\u03C5',  # υ   GREEK SMALL LETTER UPSILON
+    'phi': '\u03C6',      # φ   GREEK SMALL LETTER PHI
+    'chi': '\u03C7',      # χ   GREEK SMALL LETTER CHI
+    'psi': '\u03C8',      # ψ   GREEK SMALL LETTER PSI
+    'omega': '\u03C9',    # ω   GREEK SMALL LETTER OMEGA
+    # Greek upper case letters
+    'ALPHA': '\u0391',    # Α   GREEK CAPITAL LETTER ALPHA
+    'BETA': '\u0392',     # Β   GREEK CAPITAL LETTER BETA
+    'GAMMA': '\u0393',    # Γ   GREEK CAPITAL LETTER GAMMA
+    'DELTA': '\u0394',    # Δ   GREEK CAPITAL LETTER DELTA
+    'EPSILON': '\u0395',  # Ε   GREEK CAPITAL LETTER EPSILON
+    'ZETA': '\u0396',     # Ζ   GREEK CAPITAL LETTER ZETA
+    'ETA': '\u0397',      # Η   GREEK CAPITAL LETTER ETA
+    'THETA': '\u0398',    # Θ   GREEK CAPITAL LETTER THETA
+    'IOTA': '\u0399',     # Ι   GREEK CAPITAL LETTER IOTA
+    'KAPPA': '\u039A',    # Κ   GREEK CAPITAL LETTER KAPPA
+    'LAMDA': '\u039B',    # Λ   GREEK CAPITAL LETTER LAMDA
+    'MU': '\u039C',       # Μ   GREEK CAPITAL LETTER MU
+    'NU': '\u039D',       # Ν   GREEK CAPITAL LETTER NU
+    'XI': '\u039E',       # Ξ   GREEK CAPITAL LETTER XI
+    'OMICRON': '\u039F',  # Ο   GREEK CAPITAL LETTER OMICRON
+    'PI': '\u03A0',       # Π   GREEK CAPITAL LETTER PI
+    'RHO': '\u03A1',      # Ρ   GREEK CAPITAL LETTER RHO
+    'SIGMA': '\u03A3',    # Σ   GREEK CAPITAL LETTER SIGMA
+    'TAU': '\u03A4',      # Τ   GREEK CAPITAL LETTER TAU
+    'UPSILON': '\u03A5',  # Υ   GREEK CAPITAL LETTER UPSILON
+    'PHI': '\u03A6',      # Φ   GREEK CAPITAL LETTER PHI
+    'CHI': '\u03A7',      # Χ   GREEK CAPITAL LETTER CHI
+    'PSI': '\u03A8',      # Ψ   GREEK CAPITAL LETTER PSI
+    'OMEGA': '\u03A9',    # Ω   GREEK CAPITAL LETTER OMEGA
+    'THETA_SYMBOL': '\u03F4',  # ϴ   GREEK CAPITAL THETA SYMBOL
+}
