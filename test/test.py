4e7e99e05feb90726e6b1ab663c2a56fc45f1ae3
codeunsolved <codeunsolved@gmail.com>
2020-03-22 01:00:00 +0800

    add and update test for ngs v0.0.17

--- test/test.py
+++ test/test.py
@@ -13,7 +13,7 @@ import logging
 import argparse
 import unittest
 
-from test_ac import TestAnnCoordinates
+from test_ac import TestAnnCoordinates_GENCODE, TestAnnCoordinates_REFSEQ
 from test_mysql_cnn import TestMysqlConnector
 
 __VERSION__ = 'v0.0.1'
@@ -37,27 +37,41 @@ def parse_config(path_config):
             path_config))
 
 
-def set_mysql_attr(args):
+def set_config(args):
+    def check_if_mysql_config_is_set():
+        if args.mysql_user and args.mysql_pass and \
+           args.mysql_host and args.mysql_port and args.database:
+            return True
+        else:
+            return False
+
     config = parse_config(args.config)
 
-    for cls in [TestAnnCoordinates, TestMysqlConnector]:
-        if config:
+    # Set MySQL attributes
+    for cls in [TestAnnCoordinates_GENCODE, TestMysqlConnector]:
+        if check_if_mysql_config_is_set():
+            cls.MYSQL_USER = args.mysql_user
+            cls.MYSQL_PASS = args.mysql_pass
+            cls.MYSQL_HOST = args.mysql_host
+            cls.MYSQL_PORT = args.mysql_port
+            cls.MYSQL_DATABASE = args.mysql_database
+        elif config:
             mysql_config = getattr(config, 'MYSQL', {})
             cls.MYSQL_USER = mysql_config.get('user')
             cls.MYSQL_PASS = mysql_config.get('password')
             cls.MYSQL_HOST = mysql_config.get('host') or '127.0.0.1'
             cls.MYSQL_PORT = mysql_config.get('port') or 3306
             cls.MYSQL_DATABASE = mysql_config.get('database')
-        else:
-            cls.MYSQL_USER = args.mysql_user
-            cls.MYSQL_PASS = args.mysql_pass
-            cls.MYSQL_HOST = args.mysql_host
-            cls.MYSQL_PORT = args.mysql_port
-            cls.MYSQL_DATABASE = args.mysql_database
+    # Set AnnCoordinates RefSeq attributes
+    if args.refseq_files:
+        TestAnnCoordinates_REFSEQ.REFSEQ_FILES = args.refseq_files
+    elif config:
+        refseq_files = getattr(config, 'REFSEQ_FILES', None)
+        TestAnnCoordinates_REFSEQ.REFSEQ_FILES = refseq_files
 
 
 def set_random_seed(random_seed):
-    for cls in [TestAnnCoordinates]:
+    for cls in [TestAnnCoordinates_GENCODE, TestAnnCoordinates_REFSEQ]:
         if random_seed is None:
             cls.RANDOM_SEED = random.randint(1, 1000)
         else:
@@ -72,7 +86,8 @@ if __name__ == '__main__':
     parser.add_argument('-m', '--module', type=str, default='__main__',
                         help="Specify test module(default: all)")
     parser.add_argument('--config', type=str, default=None,
-                        help="Specify config file(.py)")
+                        help="Specify config file(.py), "
+                             "Its priority is lower than other parameters below")
     parser.add_argument('--mysql_user', type=str, default=None,
                         help="Specify MYSQL username")
     parser.add_argument('--mysql_pass', type=str, default=None,
@@ -83,6 +98,8 @@ if __name__ == '__main__':
                         help="Specify MYSQL port(default: 3306)")
     parser.add_argument('--mysql_database', type=str, default=None,
                         help="Specify MYSQL databse name")
+    parser.add_argument('--refseq_files', type=str, nargs='+',
+                        help="Specify path of RefSeq file(s)")
     parser.add_argument('--random_seed', type=int, default=None,
                         help="Specify random seed")
 
@@ -90,7 +107,7 @@ if __name__ == '__main__':
 
     logger = logging.basicConfig(level=logging.CRITICAL)  # Kepp silence
 
-    set_mysql_attr(args)
+    set_config(args)
 
     set_random_seed(args.random_seed)
 
--- test/test_ac.py
+++ test/test_ac.py
@@ -20,7 +20,7 @@ from utils.ngs import AnnCoordinates
 from utils.connector import MysqlConnector
 
 
-class TestAnnCoordinates(unittest.TestCase):
+class TestAnnCoordinates_GENCODE(unittest.TestCase):
 
     RANDOM_SEED = 0
 
@@ -66,8 +66,7 @@ class TestAnnCoordinates(unittest.TestCase):
                 mysql_config['database']))
 
         try:
-            mysql_con = MysqlConnector(
-                mysql_config)
+            mysql_con = MysqlConnector(mysql_config)
         except Exception as e:
             # Skip all test
             cls.skipTest(cls, "Invalid MySQL config: {}".format(e))
@@ -375,6 +374,344 @@ class TestAnnCoordinates(unittest.TestCase):
         self.assertEqual(gene_info['name'], 'intergenic(AC131097.4,-)')
         self.assertEqual(gene_info['type'], 'intergenic')
         self.assertEqual(gene_info['start'], 242844702)
+        self.assertEqual(gene_info['end'], 0)
+        self.assertIsNone(gene_info['source'])
+        self.assertIsNone(gene_info['strand'])
+
+
+class TestAnnCoordinates_REFSEQ(unittest.TestCase):
+    RANDOM_SEED = 0
+
+    REFSEQ_FILES = None
+
+    default_args = {
+        'mysql_config': None,
+        'ann_source': 'RefSeq',
+        'ann_refseq_files': None,  # wait to assign
+        'use_cache': True,
+        'logger_level': 50,  # logging.CRITICAL
+    }
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+    @classmethod
+    def set_random_seed(cls, random_seed):
+        print("\033[1;32m#\033[0m Random seed: \033[1;35m{}\033[0m".format(
+            random_seed))
+        np.random.seed(random_seed)
+
+    @classmethod
+    def setUpClass(cls):
+        # !important! set random seed
+        cls.set_random_seed(cls.RANDOM_SEED)
+
+        try:
+            assert isinstance(cls.REFSEQ_FILES, (str, list, set))
+        except Exception:
+            # Skip all test
+            cls.skipTest(cls, "Invalid RefSeq files: {}".format(cls.REFSEQ_FILES))
+        else:
+            cls.default_args['ann_refseq_files'] = cls.REFSEQ_FILES
+
+    @classmethod
+    def tearDownClass(cls):
+        pass
+
+    def _test_ann_alk_(self, args):
+        """
+        ALK Gene Structure
+        Chromosome: chr2
+        Genbank ID: NM_004304 Orientation: -
+        Length coding sequence : 4860 nucleotides.
+        Region  start       end         region length   phase at end
+        UTR     30,143,526  30,144,477
+        Exon    30,142,859  30,143,525  667             1
+        Exon    29,940,444  29,940,563  120             1
+        Exon    29,917,716  29,917,880  165             1
+        Exon    29,754,781  29,754,982  202             2
+        Exon    29,606,598  29,606,725  128             1
+        Exon    29,551,216  29,551,347  132             1
+        Exon    29,543,617  29,543,748  132             1
+        Exon    29,541,170  29,541,270  101             0
+        Exon    29,519,754  29,519,923  170             2
+        Exon    29,498,268  29,498,362  95              1
+        Exon    29,497,965  29,498,093  129             1
+        Exon    29,473,971  29,474,133  163             2
+        Exon    29,462,546  29,462,696  151             0
+        Exon    29,456,431  29,456,562  132             0
+        Exon    29,455,170  29,455,314  145             1
+        Exon    29,451,750  29,451,932  183             1
+        Exon    29,450,440  29,450,538  99              1
+        Exon    29,449,788  29,449,940  153             1
+        Exon    29,448,327  29,448,431  105             1
+        Exon    29,446,208  29,446,394  187             2
+        Exon    29,445,383  29,445,473  91              0
+        Exon    29,445,210  29,445,274  65              2
+        Exon    29,443,572  29,443,701  130             0
+        Exon    29,436,850  29,436,947  98              2
+        Exon    29,432,652  29,432,744  93              2
+        Exon    29,430,037  29,430,138  102             2
+        Exon    29,420,408  29,420,542  135             2
+        Exon    29,419,636  29,419,726  91              0
+        Exon    29,416,090  29,416,788  699             0
+        UTR     29,415,640  29,416,089
+        """
+        # ALK chr2:29,415,640-30,144,477(GRCh37/hg19) from GeneCards
+        # ALK chr2:29,415,640-30,144,432(GRCh37/hg19) from GENCODE/HAVANA
+        # ALK chr2:29,415,639-30,144,452(GRCh37/hg19) from RefSeq NM_004304.5
+
+        # Case01: Normal
+        ac = AnnCoordinates(**args)
+        # 5'UTR
+        ac.query('2:30,143,526')
+        gene_info = ac.gene_info
+        self.assertEqual(gene_info['id'], 'ALK')
+        self.assertEqual(gene_info['name'], 'ALK')
+        self.assertEqual(gene_info['type'], 'protein_coding')
+        self.assertEqual(gene_info['strand'], '-')
+        rank_info = ac.rank_info
+        self.assertEqual(rank_info['trans_id'], 'NM_004304.5')
+        self.assertEqual(rank_info['trans_start'], 29415639)
+        self.assertEqual(rank_info['trans_end'], 30144452)
+        self.assertEqual(rank_info['trans_strand'], '-')
+        self.assertEqual(rank_info['exon_total'], 29)
+        self.assertEqual(rank_info['cds_min'], 1)
+        self.assertEqual(rank_info['cds_max'], 29)
+        self.assertEqual(rank_info['rank'], "5'UTR")
+        self.assertEqual(rank_info['exon_num'], 1)
+        # Exon20
+        bp = np.random.choice(range(29446207, 29446394 + 1))
+        ac.query("2:{}".format(bp))
+        gene_info = ac.gene_info
+        self.assertEqual(gene_info['id'], 'ALK')
+        self.assertEqual(gene_info['name'], 'ALK')
+        self.assertEqual(gene_info['type'], 'protein_coding')
+        self.assertEqual(gene_info['strand'], '-')
+        rank_info = ac.rank_info
+        self.assertEqual(rank_info['trans_id'], 'NM_004304.5')
+        self.assertEqual(rank_info['trans_start'], 29415639)
+        self.assertEqual(rank_info['trans_end'], 30144452)
+        self.assertEqual(rank_info['trans_strand'], '-')
+        self.assertEqual(rank_info['exon_total'], 29)
+        self.assertEqual(rank_info['cds_min'], 1)
+        self.assertEqual(rank_info['cds_max'], 29)
+        self.assertEqual(rank_info['rank'], "Exon20")
+        self.assertEqual(rank_info['exon_num'], 20)
+        # Intron10
+        bp = np.random.choice(range(29498093 + 1, 29498267))
+        ac.query("2:{}".format(bp))
+        gene_info = ac.gene_info
+        self.assertEqual(gene_info['id'], 'ALK')
+        self.assertEqual(gene_info['name'], 'ALK')
+        self.assertEqual(gene_info['type'], 'protein_coding')
+        self.assertEqual(gene_info['strand'], '-')
+        rank_info = ac.rank_info
+        self.assertEqual(rank_info['trans_id'], 'NM_004304.5')
+        self.assertEqual(rank_info['trans_start'], 29415639)
+        self.assertEqual(rank_info['trans_end'], 30144452)
+        self.assertEqual(rank_info['trans_strand'], '-')
+        self.assertEqual(rank_info['exon_total'], 29)
+        self.assertEqual(rank_info['cds_min'], 1)
+        self.assertEqual(rank_info['cds_max'], 29)
+        self.assertEqual(rank_info['rank'], "Intron10")
+        self.assertEqual(rank_info['exon_num'], 10)
+        # 3'UTR
+        ac.query('2:29,415,640')
+        gene_info = ac.gene_info
+        self.assertEqual(gene_info['id'], 'ALK')
+        self.assertEqual(gene_info['name'], 'ALK')
+        self.assertEqual(gene_info['type'], 'protein_coding')
+        self.assertEqual(gene_info['strand'], '-')
+        rank_info = ac.rank_info
+        self.assertEqual(rank_info['trans_id'], 'NM_004304.5')
+        self.assertEqual(rank_info['trans_start'], 29415639)
+        self.assertEqual(rank_info['trans_end'], 30144452)
+        self.assertEqual(rank_info['trans_strand'], '-')
+        self.assertEqual(rank_info['exon_total'], 29)
+        self.assertEqual(rank_info['cds_min'], 1)
+        self.assertEqual(rank_info['cds_max'], 29)
+        self.assertEqual(rank_info['rank'], "3'UTR")
+        self.assertEqual(rank_info['exon_num'], 29)
+
+    def test_01_ann_base(self):
+        args = deepcopy(self.default_args)
+
+        self._test_ann_alk_(args)
+
+    def test_02_ann_without_cache(self):
+        args = deepcopy(self.default_args)
+
+        args['use_cache'] = False
+        self._test_ann_alk_(args)
+
+    def test_03_ann_bp_handling(self):
+        args = deepcopy(self.default_args)
+
+        ac = AnnCoordinates(**args)
+
+        ac.query('2:30,143,526')
+        self.assertEqual(ac.chr_raw, '2')
+        self.assertEqual(ac.chr, 'chr2')
+        self.assertEqual(ac.pos, 30143526)
+
+        ac.query('2:30143526')
+        self.assertEqual(ac.chr_raw, '2')
+        self.assertEqual(ac.chr, 'chr2')
+        self.assertEqual(ac.pos, 30143526)
+
+        ac.query('chr2:30,143,526')
+        self.assertEqual(ac.chr_raw, 'chr2')
+        self.assertEqual(ac.chr, 'chr2')
+        self.assertEqual(ac.pos, 30143526)
+
+        ac.query('chr2:30143526')
+        self.assertEqual(ac.chr_raw, 'chr2')
+        self.assertEqual(ac.chr, 'chr2')
+        self.assertEqual(ac.pos, 30143526)
+
+        ac.query('2', '30,143,526')
+        self.assertEqual(ac.chr_raw, '2')
+        self.assertEqual(ac.chr, 'chr2')
+        self.assertEqual(ac.pos, 30143526)
+
+        ac.query('2', '30143526')
+        self.assertEqual(ac.chr_raw, '2')
+        self.assertEqual(ac.chr, 'chr2')
+        self.assertEqual(ac.pos, 30143526)
+
+        ac.query('2', 30143526)
+        self.assertEqual(ac.chr_raw, '2')
+        self.assertEqual(ac.chr, 'chr2')
+        self.assertEqual(ac.pos, 30143526)
+
+        ac.query('chr2', '30,143,526')
+        self.assertEqual(ac.chr_raw, 'chr2')
+        self.assertEqual(ac.chr, 'chr2')
+        self.assertEqual(ac.pos, 30143526)
+
+        ac.query('chr2', '30143526')
+        self.assertEqual(ac.chr_raw, 'chr2')
+        self.assertEqual(ac.chr, 'chr2')
+        self.assertEqual(ac.pos, 30143526)
+
+        ac.query('chr2', 30143526)
+        self.assertEqual(ac.chr_raw, 'chr2')
+        self.assertEqual(ac.chr, 'chr2')
+        self.assertEqual(ac.pos, 30143526)
+
+    def test_04_ann_intergenic_gene(self):
+        args = deepcopy(self.default_args)
+
+        ac = AnnCoordinates(**args)
+
+        # intergenic(TMEM99,KRT12)
+        ac.query('chr17:39013407')
+        self.assertEqual(len(ac.gene_hits), 1)
+        gene_hit = ac.gene_hits[0]
+        self.assertIsNone(gene_hit['id'])
+        self.assertEqual(gene_hit['name'], 'intergenic(TMEM99,KRT12)')
+        self.assertEqual(gene_hit['type'], 'intergenic')
+        self.assertIsNone(gene_hit['source'])
+        self.assertIsNone(gene_hit['strand'])
+
+    def test_05_ann_intergenic_igh_gene(self):
+        args = deepcopy(self.default_args)
+
+        ac = AnnCoordinates(**args)
+        # IGHJ6 chr14:106329405-106329470
+        ac.query('chr14:106329469')
+        self.assertEqual(len(ac.gene_hits), 1)
+        gene_hit = ac.gene_hits[0]
+        # self.assertIsNone(gene_hit['id'])
+        self.assertEqual(gene_hit['name'], 'IGHJ6')
+        self.assertEqual(gene_hit['type'], 'protein_coding')
+        # self.assertIsNone(gene_hit['source'])
+        self.assertEqual(gene_hit['strand'], '-')
+        rank_info = ac.rank_info
+        self.assertEqual(rank_info['trans_strand'], '-')
+        # IGHJ3P chr14:106329624-106329675
+        # intergenic(IGHJ6,IGHJ3P)
+        ac.query('chr14:106329500')
+        self.assertEqual(len(ac.gene_hits), 1)
+        gene_hit = ac.gene_hits[0]
+        self.assertIsNone(gene_hit['id'])
+        self.assertEqual(gene_hit['name'], 'IGHJ6')
+        self.assertEqual(gene_hit['type'], 'intergenic_igh')
+        self.assertIsNone(gene_hit['source'])
+        self.assertEqual(gene_hit['strand'], '-')
+        rank_info = ac.rank_info
+        self.assertEqual(rank_info['trans_strand'], '-')
+
+    def test_06_ann_beyond_transcript_range(self):
+        args = deepcopy(self.default_args)
+
+        # BCL6 without specifying transcript map
+        ac = AnnCoordinates(**args)
+        ac.query('chr3', 187462599)
+        gene_info = ac.gene_info
+        self.assertEqual(gene_info['id'], 'BCL6')
+        self.assertEqual(gene_info['name'], 'BCL6')
+        self.assertEqual(gene_info['type'], 'protein_coding')
+        self.assertEqual(gene_info['source'], 'RefSeq')
+        self.assertEqual(gene_info['strand'], '-')
+        rank_info = ac.rank_info
+        self.assertEqual(rank_info['trans_id'], 'NM_001706.5')
+        self.assertEqual(rank_info['trans_start'], 187439168)
+        self.assertEqual(rank_info['trans_end'], 187463256)
+        self.assertEqual(rank_info['trans_strand'], '-')
+        self.assertEqual(rank_info['exon_total'], 10)
+        self.assertEqual(rank_info['cds_min'], 3)
+        self.assertEqual(rank_info['cds_max'], 10)
+        self.assertEqual(rank_info['rank'], "Intron1")
+        self.assertEqual(rank_info['exon_num'], 1)
+
+        # BCL6 with specifying transcript map
+        args['transcript_map'] = {
+            'NM_001130845'
+        }
+        ac = AnnCoordinates(**args)
+        ac.query('chr3', 187462599)
+        gene_info = ac.gene_info
+        self.assertEqual(gene_info['id'], 'BCL6')
+        self.assertEqual(gene_info['name'], 'BCL6')
+        self.assertEqual(gene_info['type'], 'protein_coding')
+        self.assertEqual(gene_info['source'], 'RefSeq')
+        self.assertEqual(gene_info['strand'], '-')
+        rank_info = ac.rank_info
+        self.assertEqual(rank_info['trans_id'], 'NM_001130845.2')
+        self.assertEqual(rank_info['trans_start'], 187439168)
+        self.assertEqual(rank_info['trans_end'], 187454289)
+        self.assertEqual(rank_info['trans_strand'], '-')
+        self.assertEqual(rank_info['exon_total'], 10)
+        self.assertEqual(rank_info['cds_min'], 3)
+        self.assertEqual(rank_info['cds_max'], 10)
+        self.assertEqual(rank_info['rank'], "intragenic")
+        self.assertEqual(rank_info['exon_num'], -5)
+
+    def test_07_ann_special_cases(self):
+        args = deepcopy(self.default_args)
+
+        # intergenic at left most side
+        ac = AnnCoordinates(**args)
+        ac.query('chr2', 100)
+        gene_info = ac.gene_info
+        self.assertIsNone(gene_info['id'])
+        self.assertEqual(gene_info['name'], 'intergenic(-,FAM110C)')
+        self.assertEqual(gene_info['type'], 'intergenic')
+        self.assertEqual(gene_info['start'], 0)
+        self.assertEqual(gene_info['end'], 38813)
+        self.assertIsNone(gene_info['source'])
+        self.assertIsNone(gene_info['strand'])
+
+        # intergenic at right most side
+        ac = AnnCoordinates(**args)
+        ac.query('chr2', 243199373 - 100)
+        gene_info = ac.gene_info
+        self.assertIsNone(gene_info['id'])
+        self.assertEqual(gene_info['name'], 'intergenic(LINC01881,-)')
+        self.assertEqual(gene_info['type'], 'intergenic')
+        self.assertEqual(gene_info['start'], 243176659)
         self.assertEqual(gene_info['end'], 0)
         self.assertIsNone(gene_info['source'])
         self.assertIsNone(gene_info['strand'])
